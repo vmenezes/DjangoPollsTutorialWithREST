a8af9802abd3cb18e529a96173c4ace503c1bf85
Victor Menezes <menezes.victor@gmail.com>
2015-07-03 02:50:17 -0400

    created Cordova/Ionic project and added "ID" to questions endpoint

--- mysite/polls/serializers.py
+++ mysite/polls/serializers.py
@@ -3,9 +3,10 @@ from rest_framework import serializers
 
 
 class QuestionSerializer(serializers.HyperlinkedModelSerializer):
+    id = Question.pk
     class Meta:
         model = Question
-        fields = ('question_text', 'pub_date')
+        fields = ('id', 'question_text', 'pub_date')
 
 class ChoiceSerializer(serializers.HyperlinkedModelSerializer):
     question = QuestionSerializer()
