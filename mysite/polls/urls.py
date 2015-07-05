31fc74ad5ef1ed317e89840d2e0e7f9ce763f9b6
Victor Menezes <menezes.victor@gmail.com>
2015-07-05 02:15:30 -0400

    allowing CRUD operations for anonymous

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -26,4 +26,17 @@ urlpatterns = [
 
 
 # The API can be accessed thru the DRF browseable interface at http://127.0.0.1:8000/polls/api/questions
-# or if thru console with the command $curl -H 'Accept: application/json; indent=4' http://127.0.0.1:8000/polls/api/questions/
+#
+# or if thru console:
+#
+# CREATE 
+# $ curl -d "question_text=api created&pub_date=2013-01-29T12:34:56.123Z" http://127.0.0.1:8000/polls/api/questions/
+#
+# RETRIEVE
+# $ curl -H 'Accept: application/json; indent=4' http://127.0.0.1:8000/polls/api/questions/
+#
+# UPDATE
+# $ curl -X PUT -d "question_text=ble ble ble&pub_date=2015-06-25T21:52:17Z" http://127.0.0.1:8000/polls/api/questions/6/
+#
+# DELETE
+# $ curl -X DELETE http://127.0.0.1:8000/polls/api/questions/5/
