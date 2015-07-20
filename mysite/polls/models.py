6b71c3c5886913d5a1d81f430adbe9a2c6d630c0
Victor Menezes <menezes.victor@gmail.com>
2015-07-19 21:31:26 -0400

    added __str__ to models

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -14,9 +14,16 @@ class Question(models.Model):
     was_published_recently.admin_order_field = 'pub_date'
     was_published_recently.boolean = True
     was_published_recently.short_description = 'Published recently?'
+    
+    def __str__(self):
+        return self.question_text
 
 
 class Choice(models.Model):
     question = models.ForeignKey(Question)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+    
+    def __str__(self): # __unicode__ on Python 2
+        return self.choice_text
+ 
